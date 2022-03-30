4afce605b1226bd6fe9f12b6b7e4dbf0d1b995d3
Ivan Logutov <foreverivan2004@gmail.com>
2022-03-30 03:08:56 +0200

    add file

---
+++ site/js/SpeakHello.js
@@ -0,0 +1,10 @@
+ (function(window){
+ 	var helloSpeaker ={};
+
+var speakWord = "Hello";
+
+helloSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+  }
+  this.helloSpeaker = helloSpeaker;
+})(window);
